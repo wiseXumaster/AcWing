0693c5de94bba6dd632f88ee8e07c2aead81fc1e
wiseXu <3058524082@qq.com>
2020-10-25 21:25:33 +0800

    null

---
+++ cpp/838.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> PII;
+const int N=1e5+10;
+int n,m;
+int h[N],sz;
+
+void down(int u){
+    int t=u;
+    if(u*2<=sz&&h[u*2]<h[t]) t=u*2;
+    if(u*2+1<=sz&&h[u*2+1]<h[t]) t=u*2+1;
+    if(u!=t){
+        swap(h[u],h[t]);
+        down(t);
+    }
+}
+
+int main(){
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++) scanf("%d",&h[i]);
+    sz=n;
+    for(int i=n/2;i;i--) down(i);
+
+    while(m--){
+        printf("%d ",h[1]);
+        h[1]=h[sz];
+        sz--;
+        down(1);
+    }
+
+    return 0;
+}
