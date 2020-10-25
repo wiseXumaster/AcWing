9416c0792b3a051c5baa478b4a34b5c67c8fb5e1
wiseXu <3058524082@qq.com>
2020-10-25 13:43:58 +0800

    ma

--- demo.cpp
+++ demo.cpp
@@ -12,7 +12,7 @@ int main(){
 
     for(int i=0;i<n;i++) scanf("%d",&a[i]);
     for(int i=0;i<m;i++) scanf("%d",&b[i]);
-    
+
     int i=0,j=0;
     for(int i=0,j=m-1;i<n;i++){
         while(j>=0&&a[i]+b[j]>x) j--;
