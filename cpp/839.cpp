567497079b5dab0bb0de79096099209f85067f71
wiseXu <3058524082@qq.com>
2020-10-25 23:52:23 +0800

    null

--- cpp/839.cpp
+++ cpp/839.cpp
@@ -2,50 +2,88 @@
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> PII;
-const int N = 1e5 + 10;
-int n, m;
-int h[N], sz;
+const int N = 1e6 + 10;
+int n, m, num;
 
-void down(int u)
+int h[N], hp[N], ph[N];
+
+void heap_swap(int a, int b)
 {
-    int t = u;
-    if (u * 2 <= sz && h[u * 2] < h[t])
-        t = u * 2;
-    if (u * 2 + 1 <= sz && h[u * 2 + 1] < h[t])
-        t = u * 2 + 1;
-    if (u != t)
-    {
-        swap(h[u], h[t]);
-        down(t);
-    }
+    swap(hp[a], hp[b]);
+    swap(ph[hp[a]], ph[hp[b]]);
+    swap(h[a], h[b]);
 }
 
 void up(int u)
 {
     while (u / 2 && h[u / 2] > h[u])
+        heap_swap(u/2, u), u /= 2;
+}
+
+void down(int u)
+{
+
+    int t = u;
+    if (u * 2 <= num && h[u * 2] < h[t])
+        t = u * 2;
+    if (u * 2 + 1 <= num && h[u * 2 + 1] < h[t])
+        t = u * 2 + 1;
+    if (u != t)
     {
-        swap(h[u], h[u / 2]);
-        u /= 2;
+        heap_swap(u, t);
+        down(t);
     }
 }
 
 int main()
 {
-    cin >> n >> m;
+    cin >> n;
 
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &h[i]);
-    sz = n;
-    for (int i = n / 2; i; i--)
-        down(i);
-
-    while (m--)
+    while (n--)
     {
-        printf("%d ", h[1]);
-        h[1] = h[sz];
-        sz--;
-        down(1);
+        char op[3];
+        int k, x;
+        scanf("%s", op);
+        if (!strcmp(op, "I"))
+        {
+            scanf("%d", &x);
+            num++;
+            m++;
+            //堆中的位置
+            ph[m] = num;
+            //位置上的点是第几个插入的
+            hp[num] = m;
+            //赋值
+            h[num] = x;
+            //up操作
+            up(num);
+        }
+        else if (!strcmp(op, "PM"))
+        {
+            printf("%d\n", h[1]);
+        }
+        else if (!strcmp(op, "DM"))
+        {
+            heap_swap(1, num);
+            num--;
+            down(1);
+        }
+        else if (!strcmp(op, "D"))
+        {
+            scanf("%d", &k);
+            k = ph[k];
+            heap_swap(k, num);
+            num--;
+            down(k), up(k);
+        }
+        else if (!strcmp(op, "C"))
+        {
+            scanf("%d%d", &k, &x);
+            k = ph[k];
+            h[k] = x;
+            down(k), up(k);
+        }
     }
-
+    system("pause");
     return 0;
 }
