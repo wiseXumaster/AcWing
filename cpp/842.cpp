6181139e3a8154225cd31d6cef3e0845f677de0b
wiseXu <3058524082@qq.com>
2020-10-29 17:12:43 +0800

    null

---
+++ cpp/842.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> PII;
+const int N=1e5+10;
+int n,m;
+int path[N];
+bool st[N];
+
+void dfs(int u){
+    if(u==n){
+        for(int i=0;i<n;i++){
+            printf("%d ",path[i]);
+        }
+        puts("");
+    }
+
+    for(int i=1;i<=n;i++){
+        if(!st[i]){
+            st[i]=true;
+            path[u]=i;
+            dfs(u+1);
+            st[i]=false;
+        }
+    }
+
+}
+
+int main(){
+    cin>>n;
+    dfs(0);
+    system("pause");
+    return 0;
+}
