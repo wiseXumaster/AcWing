3335677b6362d385026da411344c1cee7e288eaf
wiseXu <3058524082@qq.com>
2020-11-06 18:37:32 +0800

    null

--- AcWing/854.cpp
+++ AcWing/854.cpp
@@ -1,12 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-typedef pair<int,int> PII;
-const int N=1e5+10;
-int n,m;
-
+const int N=210,INF=1e9;
+int n,m,Q;
+int d[N][N];
+void floyd(){
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+}
 int main(){
+    scanf("%d%d%d",&n,&m,&Q);
 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++)
+            //自环删去
+            if(i==j) d[i][j]=0;
+            else d[i][j]=INF;
+    }
+
+    while(m--){
+        int a,b,w;
+        scanf("%d%d%d",&a,&b,&w);
+        d[a][b]=min(d[a][b],w);
+    }
+
+    floyd();
+    while(Q--){
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if(d[a][b]>INF/2) printf("impossible\n");
+        else printf("%d\n",d[a][b]);
+    }
     system("pause");
     return 0;
 }
